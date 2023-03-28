3ce5e4506b09e33d7da265c564f19e3c0ee8cc44
Owlan <106538416+MisterHow@users.noreply.github.com>
2023-03-28 21:13:35 +0100

    Added UnitTesting project. Added EntityFramework and CoinCatalogueContext for accessing localdb.

---
+++ CoinCatalogue.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CoinCatalogue.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ CoinCatalogue.WebUI/Models/Coin.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoinCatalogue.WebUI.Models
+{
+    public class CoinContext : DbContext
+    {
+        public DbSet<Coin> Coins { get; set; }
+    }
+    public class Coin
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Value { get; set; }
+        public int Year { get; set; }
+    }
+}
