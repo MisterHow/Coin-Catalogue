033f06934ed3066820ff57f58f3acbefeb45e415
Owlan <106538416+MisterHow@users.noreply.github.com>
2023-03-28 21:13:35 +0100

    Added UnitTesting project. Added EntityFramework and CoinCatalogueContext for accessing localdb.

--- CoinCatalogue.WebUI/Controllers/HomeController.cs
+++ CoinCatalogue.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using CoinCatalogue.WebUI.Models;
+using CoinCatalogue.WebUI.Models.Core;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -15,6 +16,22 @@ namespace CoinCatalogue.WebUI.Controllers
 
         public IActionResult Index()
         {
+            using (var db = new CoinCatalogueDbContext())
+            {
+                //var coin1 = new Coin { Name = "United States quarter", Value = 0.25m, Year = 1965 };
+                //var coin2 = new Coin { Name = "British pound", Value = 1.0m, Year = 1983 };
+                //var coin3 = new Coin { Name = "Canadian loonie", Value = 1.0m, Year = 1987 };
+                //var coin4 = new Coin { Name = "Mexican peso", Value = 1.0m, Year = 1910 };
+                //var coin5 = new Coin { Name = "Japanese yen", Value = 100.0m, Year = 2000 };
+                //var coin6 = new Coin { Name = "Australian dollar", Value = 1.0m, Year = 1995 };
+                //var coin7 = new Coin { Name = "Swiss franc", Value = 5.0m, Year = 2010 };
+                //var coin8 = new Coin { Name = "South African rand", Value = 1.0m, Year = 1994 };
+                //var coin9 = new Coin { Name = "French franc", Value = 10.0m, Year = 1989 };
+                //var coin10 = new Coin { Name = "Chinese yuan", Value = 10.0m, Year = 2015 };
+                //db.Coin.Add(coin);
+                //db.SaveChanges();
+            }
+
             return View();
         }
 
---
+++ CoinCatalogue.WebUI/Models/Core/CoinCatalogueContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoinCatalogue.WebUI.Models.Core
+{
+    public class CoinCatalogueDbContext : DbContext
+    {
+        public DbSet<Coin> Coin { get; set; }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=CoinCatalogue;Trusted_Connection=True;",
+                options => options.EnableRetryOnFailure(
+                maxRetryCount: 10,
+                maxRetryDelay: TimeSpan.FromSeconds(30),
+                errorNumbersToAdd: null
+            ));
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
